d6b54a5900841da3011a0b5f64442b28625cf12f
Danbro007 <710170342@qq.com>
2021-01-14 12:43:23 +0800

    修改bug

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:9001"',
+  BASE_API: '"http://localhost:8222"',
   OSS_PATH:'"https://edu-danbro.oss-cn-hangzhou.aliyuncs.com/"'
 })
 
